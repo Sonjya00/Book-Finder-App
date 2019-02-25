d27f82839c5f0294f68ba2add12c01be129ef951
Sonia Gorla <soniagorla@msn.com>
2019-02-25 02:35:54 -0600

    Updates

---
+++ precache-manifest.fd2a25591ce25b845b830530999a81b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee052b777c1b3ee7a11",
+    "url": "/Book-Finder-App/static/js/runtime~main.fee052b7.js"
+  },
+  {
+    "revision": "2dd2f782d197a03f6737",
+    "url": "/Book-Finder-App/static/js/main.2dd2f782.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/Book-Finder-App/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "2dd2f782d197a03f6737",
+    "url": "/Book-Finder-App/static/css/main.888e3209.chunk.css"
+  },
+  {
+    "revision": "1941628e8f2b217dabffe47a6f6336eb",
+    "url": "/Book-Finder-App/index.html"
+  }
+];
